12c65a1a950b980a4d9bbc21b5dbbd0b1c78070c
pickuse <pickuse2013@gmail.com>
2020-08-27 17:26:41 +0800

    change package name & add description

--- addon.js
+++ addon.js
@@ -1,4 +1,3 @@
-
 var addon = require('bindings')('addon.node')
 var opsys = process.platform;
 
@@ -7,7 +6,10 @@ exports = (folderPath) => {
     if (opsys == "darwin") {
         return addon.pathType(folderPath)
     } else if (opsys == "win32" || opsys == "win64") {
-        // TODO: 清除路徑尾巴
+        if (RegExp('^([a-z]):').test(folderPath)) {
+            folderPath = folderPath.slice(0, 3)
+        }
+
         return addon.pathType(folderPath)
     } else if (opsys == "linux") {
         throw Error('not support linux')
