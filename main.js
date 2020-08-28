b76a5918530e5f1517a75a91226564f3599048a4
pickuse <pickuse2013@gmail.com>
2020-08-28 16:09:30 +0800

    [fix] wrong regex ending

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ module.exports = (folderPath) => {
         
         // on windows they are only \\192.168.0.1 or Z:\ (mount) is network drive
         // test url is start with \\??????
-        if(RegExp('^\\\\$').test(folderPath))
+        if(RegExp('^\\\\').test(folderPath))
         {
             return true;
         }
